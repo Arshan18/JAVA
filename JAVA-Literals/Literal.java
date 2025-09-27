fd4e7b0a522d0978bd04c07e8ad1f8b1c6bee970
Arshan <arshanattar09@gmail.coom>
2025-09-27 17:42:22 +0530

    Completed Literals

---
+++ JAVA-Literals/Literal.java
@@ -0,0 +1,31 @@
+public class Literal {
+    public static void main(String[] args) {
+        int Age = 20;
+        byte Marks = 72;
+        short Std = 14;
+        long ADNo = 12345678910l;
+        char ch = 'A';
+        float f1 = 6.9f;
+        double d1 = 18.0;
+        boolean a = true;
+
+        System.out.println(Age);
+        System.out.println(Std);
+        System.out.println(ADNo);
+        System.out.println(Marks);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(a);
+
+
+
+        String str = "Hello, My name is Arshan Attar, & i am  pursuing my bachelors of engineering in Information Technology.";
+        System.out.println(str);
+
+        int Rollno = 231408;
+        System.out.println(Rollno);
+
+
+    }
+}
