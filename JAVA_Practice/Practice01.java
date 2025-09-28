0130122b860ee2730a2488b98925a6de6c1a5a67
Arshan <arshanattar09@gmail.coom>
2025-09-28 23:51:34 +0530

    Add JAVA_Practice folder with Practice01 program

---
+++ JAVA_Practice/Practice01.java
@@ -0,0 +1,41 @@
+package JAVA_Practice;
+import java.util.Scanner;
+
+public class Practice01{
+    public static void main(String[] args) {
+        System.out.println("Taking Innput From the User:");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No 1:");
+        float a = sc.nextFloat();
+        System.out.println("Enter No 2:");
+        float b = sc.nextFloat();
+        float sum = a+b;
+        System.out.println("The Sum of this Two No  is:" + sum);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
